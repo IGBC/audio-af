949d065b1bc8ad46291f8188eebada96a04d1aed
SEGFAULT <fakespamtrap@gmail.com>
2021-02-25 02:48:48 +0000

    I Remembered I am supposed to comment my code.

--- audio-af/src/buffer.rs
+++ audio-af/src/buffer.rs
@@ -3,19 +3,26 @@ use sample::ring_buffer::{Slice, SliceMut};
 
 use sample::Sample;
 
+/// A Trait for Zero-Copy Buffers. Buffers implemented using this type can be 
+/// moved freely without invoking a copy of the underlying data. The borrow
+/// checker will enforce that only one user can edit the underlying data at once
+/// These buffers however don't implement the copy trait.  
 pub trait Buffer<T>: BorrowMut<[T]> where T: Sample{
     fn new(size: usize) -> Self;
     fn size(&self) -> usize;
 }
 
+/// Buffer implementation that represents a samples in the time domain.
 pub struct TimeBuffer<T> where T: Sample {
     underlying: Box<[T]>,
 }
 
+/// Buffer implementation that represents a set of frequency bins of a signal having passed through an FFT.
 pub struct FreqBuffer<T> where T: Sample {
     underlying: Box<[T]>,
 }
 
+/// Buffer implementation that represents a set of Z Bins from a sigal having passed through a Z transform.
 pub struct ZBuffer<T> where T: Sample {
     underlying: Box<[T]>,
 }
