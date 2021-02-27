c662a2abb1081f1eb3683dec3a1caa9a021a2694
SEGFAULT <fakespamtrap@gmail.com>
2021-02-27 15:31:07 +0000

    switched over to quote (its a pain in the ass)

--- audio-af-macros/src/lib.rs
+++ audio-af-macros/src/lib.rs
@@ -1,6 +1,8 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, Result, DeriveInput, parse_str, Field};
-use proc_macro_roids::{DeriveInputStructExt, FieldExt, IdentExt};
+use syn::{parse_macro_input, DeriveInput, Field};
+use proc_macro_roids::{DeriveInputStructExt, FieldExt};
+use quote::quote;
+use syn::Ident;
 
 #[proc_macro_derive(AafBlock)]
 pub fn derive_aaf(item: TokenStream) -> TokenStream {
@@ -10,21 +12,21 @@ pub fn derive_aaf(item: TokenStream) -> TokenStream {
     let fields = input.fields();
     let parameters = fields.iter().enumerate().filter(|(_, f)| f.type_name().to_string() == "Parameter");
 
-    let mut output = format!("impl {} {{ 
-        pub fn num_parameter() -> usize {{ 
-            return {}; 
-        }}
-        
-        pub fn list_parameter(&self) -> Vec::<&'static str> {{
-            return vec![", name, parameters.clone().collect::<Vec<(usize, &Field)>>().len());
-    for (_, f) in parameters.clone() {
-        output.push_str(&format!("self.{}.get_name(), ", f.ident.clone().unwrap()));
-    }
+    let num_param = parameters.clone().collect::<Vec<(usize, &Field)>>().len();
+    let params: Vec<Ident> = parameters.clone().map(|(_,f)| {
+        f.ident.clone().unwrap()
+    }).collect();
 
+    let tokens = quote! {
+        impl #name {
+            pub fn num_parameter() -> usize {
+                return #num_param;
+            }
 
-    output.push_str("        ];
+            pub fn list_parameter(&self) -> Vec::<&'static str> {
+                return vec! [ #( self.#params.get_name(), )* ];
+            }
         }
-    }");
-    let tokens = output.parse().unwrap();
-    tokens
+    };
+    TokenStream::from(tokens)
 }
